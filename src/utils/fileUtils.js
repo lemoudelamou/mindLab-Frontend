887116e31fad1dfc2dea0b8ddaeddb66cf2871f7
s91072 <s91072@bht-berlin.de>
2023-11-28 09:37:04 +0100

    delete unused files

--- src/utils/fileUtils.js
+++
@@ -1,7 +0,0 @@
-// fileUtils.js
-import { saveAs } from 'file-saver';
-
-export const saveToFile = (data) => {
-  const blob = new Blob([JSON.stringify(data)], { type: 'application/json;charset=utf-8' });
-  saveAs(blob, 'experiment_results.json');
-};
--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const debounce = (func, delay) => {
-  let timeoutId;
-  return function (...args) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(this, args), delay);
-  };
-};
